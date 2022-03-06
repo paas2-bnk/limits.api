a3e3420cd52e875e7c423d2bde54c9d8068a6e3f
Turkel Gadirzade <gadirzade@gmail.com>
2022-03-06 14:10:22 +0400

    first commit

---
+++ Controllers/LimitsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Limits.API.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{v:apiVersion}")]
+    [ApiController]
+    public class LimitsController : ControllerBase
+    {
+        [HttpGet("limits")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetLimit([FromQuery] string cif)
+        {
+            var headers = Request.Headers.ToDictionary(k => k.Key, v => v.Value.First());
+
+            if (headers.Any())
+            {
+                throw new Exception(string.Join(",", headers.Select(x => x.Value)));
+            }
+
+            return Ok(new
+            {
+                DailyLimit = 100,
+                MonthlyLimit = 1000,
+                WeeklyLimit = 5000
+            });
+        }
+
+        [HttpPost("limits")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create([FromBody] UpdateLimitRequest request)
+        {
+            return Ok();
+        }
+    }
+}
